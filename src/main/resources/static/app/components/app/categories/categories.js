6be93043a593ab839db204f7ae6afeb9edbd2a09
Rodrigo <rodrigosfribeiro@gmail.com>
2016-05-24 10:56:54 +0100

    angularJs Detail

--- src/main/resources/static/app/components/app/categories/categories.js
+++ src/main/resources/static/app/components/app/categories/categories.js
@@ -2,12 +2,20 @@ var categories = angular.module('categories', [])
 
 .component('categories', {
 	template : '<h2>Categories</h2><ng-outlet></ng-outlet>',
-	$routeConfig : [ {
+	$routeConfig : [ 
+	{
 		path : '/',
 		name : 'CategoryList',
 		component : 'categoryList',
 		useAsDefault : true
-	} ]
+	},
+	{
+		path : '/:id',
+		name : 'CategoryDetail',
+		component : 'categoryDetail',
+	}
+	
+	]
 })
 
 .component('categoryList', {
@@ -15,34 +23,63 @@ var categories = angular.module('categories', [])
 	controller : CategoryListComponent
 })
 
-function CategoryListComponent() {
+.component('categoryDetail', {
+	templateUrl : "app/components/app/categories/categoryDetail.html",
+	controller : CategoryDetailComponent
+})
+
+
+function CategoryService($q){
+	var categoriesPromise = $q.when([ {
+		id : 11,
+		name : 'Requirements'
+	}, {
+		id : 12,
+		name : 'Test'
+	}, {
+		id : 13,
+		name : 'Refactor'
+	}, {
+		id : 14,
+		name : 'Design'
+	} ]);
+
+	this.getCategories = function() {
+		return categoriesPromise;
+	};
+	
+	this.getCategory = function(id) {
+		return categoriesPromise.then(function(categories){
+			for(var i =0; i<categories.lenght;i++) {
+				if(categories[i].id == id) return categories[i];
+			}
+		})
+	};
+	
+}
+
+function CategoryListComponent(categoryService) {
 	var $ctrl = this;
 
 	this.$routerOnActivate = function(next) {
-		service.getCategories().then(function(heroes) {
+		categoryService.getCategories().then(function(categories) {
 			$ctrl.categories = categories;
 		});
 	};
 
-	this.service = function($q) {
-		var heroesPromise = $q.when([ {
-			id : 11,
-			name : 'Requirements'
-		}, {
-			id : 12,
-			name : 'Test'
-		}, {
-			id : 13,
-			name : 'Refactor'
-		}, {
-			id : 14,
-			name : 'Design'
-		} ]);
+}
 
-		this.getCategories = function() {
-			return categoriesPromise;
+function CategoryDetailComponent(categoryService){
+	var $ctrl = this;
+	
+	this.$routerOnActivate = function(next, previous){
+		var id = next.params.id;
+		  return categoryService.getCategory(id).then(function(categories) {
+		    $ctrl.category = category;
+		  });
+	};
+	
+	this.gotoCategory = function() {
+		  this.$router.navigate(['CategoryList']);
 		};
-
-	}
-
 }
